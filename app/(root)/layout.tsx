249c0a81d881ff7a9c8b7ea0c0dfe04cbdb19c49
Victor Alvar <victor.alvar@tutanota.com>
2024-06-01 15:02:35 -0300

    Add update packages and clerk configuration.

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,7 +3,7 @@ import {Inter} from 'next/font/google'
 import {ClerkProvider} from '@clerk/nextjs'
 import {dark} from '@clerk/themes'
 
-import './globals.css'
+import '../globals.css'
 
 const inter = Inter({subsets: ['latin']})
 
